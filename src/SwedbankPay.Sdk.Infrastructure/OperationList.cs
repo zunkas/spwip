9d31de9469a149a604aa8c9cac2742a53ee5b079
Daniel Sundqvist <daniel.sundqvist@authority.se>
2023-12-19 15:00:31 +0100

    Add new files for different payment order states in SwedbankPay SDK
    Extended functionality of the SwedbankPay SDK by adding new interfaces, enumerators and classes that encapsulate information about different states of the payment order. This includes data about failed attempts, order items, payment URLs, and different types of responses. The addition of these new components enables better tracking and processing of various states and results associated with payment orders.

---
+++ src/SwedbankPay.Sdk.Infrastructure/OperationList.cs
@@ -0,0 +1,6 @@
+namespace SwedbankPay.Sdk.Infrastructure;
+
+internal class OperationList : List<HttpOperation>, IOperationList
+{
+    
+}
---
+++ src/SwedbankPay.Sdk.Infrastructure/OperationListDto.cs
@@ -0,0 +1,39 @@
+namespace SwedbankPay.Sdk.Infrastructure;
+
+internal class OperationListDto : List<HttpOperationDto>
+{
+    public OperationListDto()
+    {
+    }
+
+    public OperationListDto(IEnumerable<HttpOperationDto> operations)
+        : base(operations)
+    {
+    }
+
+    public override string ToString()
+    {
+        return string.Join(", ", this.Select(o => o.Rel));
+    }
+
+    public IOperationList Map()
+    {
+        var list = new OperationList();
+        foreach (var item in this)
+        {
+            var rel = new LinkRelation(item.Rel);
+            var href = new Uri(item.Href, UriKind.RelativeOrAbsolute);
+            list.Add(new HttpOperation(href, rel, item.Method, item.ContentType));
+        }
+
+        return list;
+    }
+}
+
+internal record HttpOperationDto
+{
+    public string? ContentType { get; init; }
+    public string Href { get; init; } = null!;
+    public string? Method { get; init; }
+    public string? Rel { get; init; }
+}
